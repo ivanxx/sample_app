4fe1029f2b1a7e294cc0853a235c22ca63274bd0
ivanxx <ivan@monodosis.com>
2014-03-09 21:44:02 +0100

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "UserPages" do
+  include Rails.application.routes.url_helpers  
+  subject { page }
+  
+  describe "Signup page" do
+  	before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+end
